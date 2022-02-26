1acdf189c3688741d65a3fcd67be10fd880eec95
Sammuel <danielsamm62@gmail.com>
2022-02-25 22:40:59 -0600

    Comunicación entre componentes

--- src/AddCategory.js
+++ src/AddCategory.js
@@ -1,16 +1,20 @@
 import React, { useState } from 'react'
+import PropTypes from 'prop-types';
+export const AddCategory = ({setCategories}) => {
 
-export const AddCategory = () => {
-
-    const [inputValue, setInputValue] = useState('Hola mundo');
+    const [inputValue, setInputValue] = useState('');//''
 
     const hadleInputChange = (e) => {
         setInputValue(e.target.value);
     }
 
     const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log('Submit hecho')
+        e.preventDefault(); 
+
+        if (inputValue.trim().length > 2) {
+            setCategories(cats => [...cats, inputValue ] );
+            setInputValue('');
+        }
 
     }
 
@@ -24,4 +28,7 @@ export const AddCategory = () => {
             />
       </form>
   )
+}
+AddCategory.propTypes = {
+    setCategories: PropTypes.func.isRequired
 }
